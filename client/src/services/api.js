b6cc82282d8fec8f3529af7b35d21ecf14256b08
Ritesh Kumar <riteshkumar04294@gmail.com>
2025-06-20 16:25:58 +0530

    second commit

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -2,7 +2,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://public-media-backend.onrender.com/api",
 });
 
 // Upload media
